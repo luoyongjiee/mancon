f048bacf1b8dece3be5c1fc86365470e4aaa31a5
luoqi <136428148@qq.com>
2016-04-10 20:33:53 +0800

    修改excel

---
+++ src/main/java/com/efunds/market/mc/dao/iface/UserInfoModelMapper.java
@@ -0,0 +1,22 @@
+package com.efunds.market.mc.dao.iface;
+
+import com.efunds.market.mc.dao.model.UserInfoModel;
+import com.efunds.market.mc.dao.model.UserInfoModelExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserInfoModelMapper {
+    int countByExample(UserInfoModelExample example);
+
+    int deleteByExample(UserInfoModelExample example);
+
+    int insert(UserInfoModel record);
+
+    int insertSelective(UserInfoModel record);
+
+    List<UserInfoModel> selectByExample(UserInfoModelExample example);
+
+    int updateByExampleSelective(@Param("record") UserInfoModel record, @Param("example") UserInfoModelExample example);
+
+    int updateByExample(@Param("record") UserInfoModel record, @Param("example") UserInfoModelExample example);
+}
